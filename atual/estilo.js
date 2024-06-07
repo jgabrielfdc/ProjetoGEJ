0f5978e253fbe4744de6e029352e021bed47772d
Jonas Kunzler <k.jonasaugusto@gmail.com>
2024-06-07 16:44:44 -0300

    fix: rename folder and separate login and signup pages from authentication

---
+++ atual/estilo.js
@@ -0,0 +1,10 @@
+function login(){
+    document.getElementById('cadastro').style.display='none'
+    document.getElementById('login').style.display=''
+}
+
+function cadastro(){
+    document.getElementById('login').style.display='none'
+    document.getElementById('cadastro').style.display=''
+    document.getElementById('cadastro').style.transition='1s all'
+}
